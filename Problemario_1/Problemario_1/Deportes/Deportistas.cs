f43e9faba3a53f2fa25edd71bf4b528253a0bf01
Ruben Catalan Gutierrez <byrubenss@gmail.com>
2019-11-15 23:39:06 -0600

    Crud

---
+++ Problemario_1/Problemario_1/Deportes/Deportistas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Problemario_1.Deportes
+{
+    class Deportistas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Deporte { get; set; }
+        public string Horario { get; set; }
+        public string Sexo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Deporte} - {Horario}-{Sexo}";
+        }
+    }
+}
---
+++ Problemario_1/Problemario_1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Problemario_1.Deportes;
+using SQLite;
+
+namespace Problemario_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Deportistas jugador = new Deportistas()
+            {
+                Nombre = txtBNombre.Text,
+                Deporte = txtBDeporte.Text,
+                Sexo = txtBSexo.Text,
+                Horario = txtBHorario.Text
+            };
+            using (SQLiteConnection conexion = new SQLiteConnection(App.databasePath))
+            {
+                conexion.CreateTable<Deportistas>();
+                conexion.Insert(jugador);
+            }
+            Close();
+        }
+    }
+}
